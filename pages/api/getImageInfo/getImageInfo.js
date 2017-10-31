27ca909a3355c2ce90c316d2bd27a9ef41e626db
wangliu <liuwang@yiche.com>
2017-10-31 11:16:45 +0800

    added new pages.

---
+++ pages/api/getImageInfo/getImageInfo.js
@@ -0,0 +1,54 @@
+// pages/api/getImageInfo/getImageInfo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    width: 0,
+    height: 0,
+    path: ''
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  getImageFileInfo: function (e) {
+    var self = this;
+    wx.getImageInfo({
+      src: 'http://image.bitautoimg.com/appimage/cms/20170816/w750_h360_818289c40b984e098aff7fdc1fa68bcf.jpg',
+      success: function (res) {
+        console.log(res.width);
+        console.log(res.height);
+
+        self.setData({
+          width: res.width,
+          height: res.height,
+          path: res.path
+        });
+      }
+    });
+
+    // wx.chooseImage({
+    //   success: function (res) {
+    //     wx.getImageInfo({
+    //       src: res.tempFilePaths[0],
+    //       success: function (picInfo) {
+    //         console.log(picInfo.height);
+    //         console.log(picInfo.width);
+    //         console.log(picInfo.path);
+    //         //更新UI
+    //         self.setData({
+    //           width: picInfo.width,
+    //           height: picInfo.height,
+    //           path: picInfo.path
+    //         });
+    //       }
+    //     });
+    //   }
+    // });
+  }
+})
---
+++ pages/api/previewImage/previewImage.js
@@ -0,0 +1,32 @@
+// pages/api/previewImage/previewImage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  /**
+   * 
+   * 
+   * 预览图片文件
+   */
+  previewImageFile: function (e) {
+    wx.previewImage({
+      current: 'http://image.bitautoimg.com/appimage/cms/20170816/w750_h360_818289c40b984e098aff7fdc1fa68bcf.jpg',
+      urls: [
+        'http://image.bitautoimg.com/appimage/cms/20170929/w750_h360_d8773ab7d65b48f4a899ba5fd1cbe401.png', 'http://image.bitautoimg.com/appimage/cms/20170816/w750_h360_3d337984a4334ad5aee940644fa86c87.jpg',
+        'http://image.bitautoimg.com/appimage/cms/20170816/w750_h360_c3a8ad8ab07c4857a9bfdfc638e8dca1.jpg',
+        'http://image.bitautoimg.com/appimage/cms/20170816/w750_h360_818289c40b984e098aff7fdc1fa68bcf.jpg'
+      ]
+    })
+  }
+})
