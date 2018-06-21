75857a13e9d1d46ed7f5fc2528f499346eafa616
JasonLiu001 <liuwang@yiche.com>
2018-06-21 16:32:48 +0800

    列表布局初始化

---
+++ app/utils/fakedata.js
@@ -0,0 +1,14 @@
+const tableFields = ['field1', 'field2', 'field3', 'field4', 'field5', 'field6', 'field7', 'field8'];
+
+// 图标列表
+const iconList = {
+  checkbox_checked: 'https://cnlands.cn/images/icon_box-checked.png',
+  checkbox_unchecked: 'https://cnlands.cn/images/icon_box-empty.png',
+  icon_completed_right: 'https://cnlands.cn/images/icon_right.png',
+  icon_completed_wrong: 'https://cnlands.cn/images/icon_error.png'
+};
+
+module.exports = {
+  tableFields: tableFields,
+  iconList: iconList
+}
