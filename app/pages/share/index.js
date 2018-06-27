1116bba3686f6d56df2a39788ce787c0140ad9c2
JasonLiu001 <liuwang@yiche.com>
2018-06-27 17:28:41 +0800

    添加测试log输出

--- app/pages/share/index.js
+++ app/pages/share/index.js
@@ -5,13 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+    console.log('onLoad invoked..');
     if (options && options.sourcefromurl) {
       wx.navigateTo({
         url: decodeURIComponent(options.sourcefromurl)
@@ -30,7 +31,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-   
+    console.log('onShow invoked..');
   },
 
   /**
@@ -65,7 +66,11 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function() {
-
+    return {
+      title: '首页',
+      path: this.route,
+      imageUrl: 'https://www.cnlands.cn/images/icon_error.png'
+    };
   },
 
   openDetailPage: function(evt) {
